0e8ee3724aa1c16174eca4e851130119a51964c7
kevinreber <kevinreber1@gmail.com>
2023-11-18 21:02:52 -0800

    Initialized tailwind with Remix v2

--- app/root.tsx
+++ app/root.tsx
@@ -9,8 +9,17 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import stylesheet from "~/styles/tailwind.css";
+
+console.log(stylesheet);
+
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  ...(cssBundleHref
+    ? [
+        { rel: "stylesheet", href: cssBundleHref },
+        { rel: "stylesheet", href: stylesheet },
+      ]
+    : []),
 ];
 
 export default function App() {
